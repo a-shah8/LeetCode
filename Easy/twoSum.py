0b8dccbcc9fbdaf0172da933b7c3b2b5f26307d4
Abhimanyu Shah <55104146+a-shah8@users.noreply.github.com>
2021-06-19 17:36:37 -0400

    Use of hash map to store num and index

---
+++ Easy/twoSum.py
@@ -0,0 +1,12 @@
+# Return indices to two numbers whose sum equals target
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        hashmap = {}
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[nums[i]] = i
