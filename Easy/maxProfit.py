8d88db76a4ba0b6f092d68cdba50a5050f80acb9
Abhimanyu Shah <55104146+a-shah8@users.noreply.github.com>
2021-06-21 14:57:39 -0400

    Create maxProfit.py

---
+++ Easy/maxProfit.py
@@ -0,0 +1,14 @@
+## Best Time to Buy and Sell Stock
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        min_price, max_profit = inf, 0
+        
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            elif prices[i]-min_price > max_profit:
+                max_profit = prices[i] - min_price
+                
+        return max_profit
