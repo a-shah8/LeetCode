f6a22d89cd610f61ebb3ea5dc9df1ffbc7e1254e
Abhimanyu Shah <55104146+a-shah8@users.noreply.github.com>
2021-09-08 14:29:06 -0400

    876. Middle of the Linked List - 2 pointers slow & fast

---
+++ Easy/876_middleNode.py
@@ -0,0 +1,24 @@
+## Using 2 extra pointers - slow and fast
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        fast = head
+        while fast and fast.next:
+            head = head.next
+            fast = fast.next.next
+            
+        return head
+    
+#         # Using 2 extra pointers slow and fast
+#         slow = fast = head
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+            
+#         return slow
