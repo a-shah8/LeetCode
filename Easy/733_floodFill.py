ea622302413a36f2d6888cec9ec4dd60e796511a
Abhimanyu Shah <55104146+a-shah8@users.noreply.github.com>
2021-09-12 19:36:34 -0400

    733. Flood Fill - using recursion

---
+++ Easy/733_floodFill.py
@@ -0,0 +1,23 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        
+        # Using Recursion
+        
+        row, col = len(image), len(image[0])
+        color = image[sr][sc]
+        
+        if color==newColor:
+            return image
+        
+        def dfs(r, c):
+            
+            if image[r][c] == color:
+                image[r][c] = newColor
+                
+                if r >= 1: dfs(r-1, c)
+                if r+1 < row: dfs(r+1, c)
+                if c >= 1: dfs(r, c-1)
+                if c+1 < col: dfs(r, c+1)
+                    
+        dfs(sr, sc)    
+        return image
